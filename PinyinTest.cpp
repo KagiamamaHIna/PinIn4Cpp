2ee8d07756a83e78149e754e067955f7c7337551
KagiamamaHIna <kagiyamahino@gmail.com>
2025-07-14 18:44:53 +0800

    移除不必要文件和稍微修改下PinyinTest文件

--- PinyinTest.cpp
+++ PinyinTest.cpp
@@ -1,35 +1,26 @@
+/*
+	本文件只是简单的拼音测试用例，不是核心代码库之一
+	你如果想要使用搜索功能，你应该用TreeSearcher.h获取拼音匹配支持
+	如果你只需要拼音获取，那么本项目的PinIn也是非常合适的
+*/
+
 #include <iostream>
 #include "PinIn.h"
 #include <fstream>
-#include "TreeSearcher.h"
 #include <chrono>
+
+#include "TreeSearcher.h"
 
 static long long GetTimestampMS() {//获取当前毫秒的时间戳
 	auto now = std::chrono::steady_clock::now();
 	//将时间点转换为时间戳
 	return std::chrono::time_point_cast<std::chrono::milliseconds>(now).time_since_epoch().count();
 }
-#include "Keyboard.h"
-/*
-TODO:
-拼音格式化类应该用一个函数实现，其重量级的实现放在cpp文件中，用枚举类确定其行为
-*/
+
 int main() {
-	system("chcp 65001");
+	system("chcp 65001");//编码切换
 
-	/*size_t id1 = test.put("你好！");
-	size_t id2 = test.put("草uckyou!");
-	size_t id3 = test.put("逆天");
-
-	std::cout << id1 << '\n';
-	std::cout << id2 << '\n';
-	std::cout << id3 << '\n';
-
-	std::cout << test.getstr_view(id2);
-
-	PinInCpp::PinIn testPin("D:/repos/PinyinTest/pinyin.txt");*/
-	system("pause");
-	PinInCpp::TreeSearcher tree(PinInCpp::Logic::CONTAIN, "D:/repos/PinyinTest/pinyin.txt");
+	PinInCpp::TreeSearcher tree(PinInCpp::Logic::CONTAIN, "pinyin.txt");
 	PinInCpp::PinIn::Config cfg = tree.GetPinIn().config();
 	cfg.fZh2Z = true;
 	cfg.fSh2S = true;
@@ -38,10 +29,10 @@ int main() {
 	cfg.fIng2In = true;
 	cfg.fEng2En = true;
 	cfg.fU2V = true;
-	cfg.fFirstChar = true;
+	cfg.fFirstChar = true;//新增的首字母模糊
 	cfg.commit();
 
-	std::fstream file("D:/repos/PinyinTest/small.txt");
+	std::fstream file("small.txt");
 	std::string line;
 	long long now = GetTimestampMS();
 	while (std::getline(file, line)) {
@@ -49,8 +40,11 @@ int main() {
 	}
 	long long end = GetTimestampMS();
 	std::cout << end - now << '\n';
+	//插入耗时，比Java的慢，主要原因还是在utf8字符串处理之类的问题上，当然内存占用也是如此(更大)，utf8比utf16浪费内存
+	//项目统一用标准的std::string和std::string_view处理字符串，包括utf8字符串
+	//所以这是一个天然支持utf8且只支持utf8的库，原始的Java PinIn库有个不可忽视的缺陷，就是不支持utf16不可表达的字
 
-	while (true) {
+	while (true) {//死循环，你可以随便搜索测试集的内容用于测试
 		std::getline(std::cin, line);
 		now = GetTimestampMS();
 		auto vec = tree.ExecuteSearchView(line);
@@ -58,35 +52,6 @@ int main() {
 		for (const auto& v : vec) {
 			std::cout << v << '\n';
 		}
-		std::cout << end - now << '\n';
+		std::cout << end - now << '\n';//计算获取耗时并打印，单位毫秒
 	}
-
-
-	/*std::cout << (b[0] == '\0') << '\n';*/
-
-	/*PinInCpp::PinIn::Character Char = testPin.GetChar("栓");
-	for (const auto& py : Char.GetPinyins()) {
-		std::cout << py.ToString() << '\n';
-	}*/
-	/*auto cfg = testPin.config();
-	cfg.keyboard = PinInCpp::Keyboard::DAQIAN;
-	cfg.commit();
-
-	auto vec = testPin.GetPinyinView("圈", true);
-
-	for (const auto& str : vec) {
-		std::cout << str << ':';
-		for (const auto& s : testPin.getkeyboard().split(str)) {
-			std::cout << s << ' ';
-		}
-		std::cout << std::endl;
-	}*/
-
-	//PinInCpp::PinIn testPin("D:/repos/PinyinTest/pinyin.txt");
-	//for (const auto& str : testPin.GetPinyinView("佻", true)) {
-	//	std::cout << str << '\n';
-	//}
-
-
-	system("pause");
 }
