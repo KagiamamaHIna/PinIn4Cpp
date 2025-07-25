8bcdb547bb5e0c89646efcf8ded537676695ad7d
KagiamamaHIna <kagiyamahino@gmail.com>
2025-07-25 22:19:53 +0800

    对象池设计更加现代化

--- ObjectPool.h
+++ ObjectPool.h
@@ -66,56 +66,27 @@ namespace PinInCpp {
 		ObjectPool& operator=(ObjectPool&&) = delete;
 
 		template<typename... _Types>
-		std::unique_ptr<T, std::function<void(T*)>> NewObj(_Types&&..._Args) {
+		std::unique_ptr<T, std::function<void(T*)>> MakeUnique(_Types&&..._Args) {//创建一个独占所有权的智能指针
 			std::shared_ptr<bool> IsDestructionPtr = IsDestruction;//先把所有权共享给函数局部
 			auto delFn = [this, IsDestructionPtr](T* ptr) {//传递共享所有权的智能指针进去，确保他能被通知自己管理的内存是否被析构了
 				if (!(*IsDestructionPtr)) {//如果没有被析构
 					this->FreeToPool(ptr);
 				}
 				};
-			if (FreeList.empty()) {//如果对象池空闲，那么就新建
-				if (nextpos >= OnePoolSize) {//如果没有空间了，就分配新的一块并重置nextpos
-					pool.push(std::array<Block, OnePoolSize>());//压入一块新内存
-					nextpos = 0;
-				}
-				std::array<Block, OnePoolSize>& arr = pool.top();
-				T* result = reinterpret_cast<T*>(arr.data() + nextpos);
-				new (result) T(std::forward<_Types>(_Args)...);
-				nextpos++;//标记位移动延迟到构造完成，如果构造函数抛出异常，则相当于没移动，下一次依旧可以用，如果没有异常则正常移动，避免try块的设计
-				return std::unique_ptr<T, std::function<void(T*)>>(result, delFn);//通过RVO/移动构造之类的形式，转移这个智能指针的所有权
-			}
-			else {//不空闲，就从对象池中取一个标记为要析构的对象，用placement new重新构造后转移所有权
-				T* result;
-				if (lastRenewUnfinished == NotUnfinished) {
-					//这里有可能会被vs2022的静态分析报警告 "忽略函数返回值"，但是析构函数没有返回值，所以是误报
-					//延迟到这里析构，主要是方便ObjectPool的析构函数实现
-					result = FreeList[FreeList.size() - 1];
-					result->~T();
-				}
-				else {
-					result = FreeList[lastRenewUnfinished];
-				}
-
-				try {
-					new (result) T(std::forward<_Types>(_Args)...);
-				}
-				catch (...) {
-					if (lastRenewUnfinished == NotUnfinished) {//检查是否需要标记，如果需要则就用下面的逻辑，不需要就按原样，等待下一次构造
-						lastRenewUnfinished = FreeList.size() - 1;//标记renew失败的索引，让下一次重新构造的时候优先选择这个内存
-					}
-					throw;
-				}
-
-				if (lastRenewUnfinished != NotUnfinished) {
-					//代表的是复用曾经构造异常的内存，这时候针对性移除元素
-					//这里我们用一个巧妙的实现，来快速交换lastRenewUnfinished标记元素和尾部元素，实现删除的O1操作
-					std::swap(FreeList[lastRenewUnfinished], FreeList[FreeList.size() - 1]);
-					lastRenewUnfinished = NotUnfinished;//标记复用成功，即 没有未完成 状态
-				}
-				FreeList.pop_back();//将这段代码放到placement new之后，如果T构造函数异常了，则不弹出空闲列表
-				return std::unique_ptr<T, std::function<void(T*)>>(result, delFn);//通过RVO/移动构造之类的形式，转移这个智能指针的所有权
-			}
+			return std::unique_ptr<T, std::function<void(T*)>>(NewObj(std::forward<_Types>(_Args)...), delFn);//通过RVO/移动构造之类的形式，转移这个智能指针的所有权
 		}
+
+		template<typename... _Types>
+		std::shared_ptr<T> MakeShared(_Types&&..._Args) {//创建一个共享所有权的智能指针
+			std::shared_ptr<bool> IsDestructionPtr = IsDestruction;//先把所有权共享给函数局部
+			auto delFn = [this, IsDestructionPtr](T* ptr) {//传递共享所有权的智能指针进去，确保他能被通知自己管理的内存是否被析构了
+				if (!(*IsDestructionPtr)) {//如果没有被析构
+					this->FreeToPool(ptr);
+				}
+				};
+			return std::shared_ptr<T>(NewObj(std::forward<_Types>(_Args)...), delFn);//通过RVO/移动构造之类的形式，转移这个智能指针的所有权
+		}
+
 		void FreeListShrinkToFit() {
 			FreeList.shrink_to_fit();
 		}
@@ -158,6 +129,52 @@ namespace PinInCpp {
 				nextpos = OnePoolSize;//如果下一次还有，则从下一次的顶部开始指定，结束后也是置顶的，确保潜在的下一次分配生效
 				pool.pop();
 			}//析构函数手动执行完成后，剩下的内存块就可以安心交给STL
+		}
+
+		template<typename... _Types>
+		T* NewObj(_Types&&..._Args) {//目标是尝试构造一个对象并返回其裸指针
+			if (FreeList.empty()) {//如果对象池空闲，那么就新建
+				if (nextpos >= OnePoolSize) {//如果没有空间了，就分配新的一块并重置nextpos
+					pool.push(std::array<Block, OnePoolSize>());//压入一块新内存
+					nextpos = 0;
+				}
+				std::array<Block, OnePoolSize>& arr = pool.top();
+				T* result = reinterpret_cast<T*>(arr.data() + nextpos);
+				new (result) T(std::forward<_Types>(_Args)...);
+				nextpos++;//标记位移动延迟到构造完成，如果构造函数抛出异常，则相当于没移动，下一次依旧可以用，如果没有异常则正常移动，避免try块的设计
+				return result;
+			}
+			else {//不空闲，就从对象池中取一个标记为要析构的对象，用placement new重新构造后转移所有权
+				T* result;
+				if (lastRenewUnfinished == NotUnfinished) {
+					//这里有可能会被vs2022的静态分析报警告 "忽略函数返回值"，但是析构函数没有返回值，所以是误报
+					//延迟到这里析构，主要是方便ObjectPool的析构函数实现
+					result = FreeList[FreeList.size() - 1];
+					result->~T();
+				}
+				else {
+					result = FreeList[lastRenewUnfinished];
+				}
+
+				try {
+					new (result) T(std::forward<_Types>(_Args)...);
+				}
+				catch (...) {
+					if (lastRenewUnfinished == NotUnfinished) {//检查是否需要标记，如果需要则就用下面的逻辑，不需要就按原样，等待下一次构造
+						lastRenewUnfinished = FreeList.size() - 1;//标记renew失败的索引，让下一次重新构造的时候优先选择这个内存
+					}
+					throw;
+				}
+
+				if (lastRenewUnfinished != NotUnfinished) {
+					//代表的是复用曾经构造异常的内存，这时候针对性移除元素
+					//这里我们用一个巧妙的实现，来快速交换lastRenewUnfinished标记元素和尾部元素，实现删除的O1操作
+					std::swap(FreeList[lastRenewUnfinished], FreeList[FreeList.size() - 1]);
+					lastRenewUnfinished = NotUnfinished;//标记复用成功，即 没有未完成 状态
+				}
+				FreeList.pop_back();//将这段代码放到placement new之后，如果T构造函数异常了，则不弹出空闲列表
+				return result;
+			}
 		}
 		std::vector<T*> FreeList;
 		std::stack<std::array<Block, OnePoolSize>> pool;
